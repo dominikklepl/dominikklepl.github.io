b17db748c0dfac62e3cef3b6b39484fe443a40a4
Dominik Klepl <dominikklepl@gmail.com>
2019-03-09 18:45:07 +0100

    background responsive now

--- js/typewriter.js
+++ js/typewriter.js
@@ -52,6 +52,6 @@ var TxtType = function(el, toRotate, period) {
         // INJECT CSS
         var css = document.createElement("style");
         css.type = "text/css";
-        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #4f4f4f}";
         document.body.appendChild(css);
     };
